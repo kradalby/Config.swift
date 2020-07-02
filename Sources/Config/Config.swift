b7c69de3556a683cf1f8146fe6737a44a3c135ac
Kristoffer Dalby <kradalby@kradalby.no>
2020-07-02 18:01:14 +0100

    Reformat and bump tools

--- Sources/Config/Config.swift
+++ Sources/Config/Config.swift
@@ -8,38 +8,37 @@ public protocol Configuration: Decodable {
 }
 
 public func readConfig<T: Configuration>(configFormat: T.Type, atPath: String) -> T {
-    if let config = readAndDecodeJsonFile(configFormat, atPath: atPath) {
-        return config
-    }
-    log.error("Could not read configuration file")
-    exit(1)
+  if let config = readAndDecodeJsonFile(configFormat, atPath: atPath) {
+    return config
+  }
+  log.error("Could not read configuration file")
+  exit(1)
 }
 
+func readAndDecodeJsonFile<T>(_ type: T.Type, atPath: String) -> T? where T: Decodable {
+  let fm = FileManager()
+  var isDirectory: ObjCBool = ObjCBool(false)
+  let exists = fm.fileExists(atPath: atPath, isDirectory: &isDirectory)
 
-func readAndDecodeJsonFile<T>(_ type: T.Type, atPath: String) -> T? where T : Decodable {
-    let fm = FileManager()
-    var isDirectory: ObjCBool = ObjCBool(false)
-    let exists = fm.fileExists(atPath: atPath, isDirectory: &isDirectory)
-    
-    if exists && !isDirectory.boolValue {
-        if let indexFile = try? Data(contentsOf: URL(fileURLWithPath: atPath)) {
-            
-            log.info("Decoding \(atPath)")
-            let decoder = JSONDecoder()
-            if #available(OSX 10.12, *) {
-                decoder.dateDecodingStrategy = .iso8601
-            }
-            
-            if let decodedData = try? decoder.decode(type, from: indexFile) {
-                return decodedData
-            } else {
-                log.error("Could not decode \(atPath)")
-            }
-        } else {
-            log.error("Could not read \(atPath)")
-        }
+  if exists && !isDirectory.boolValue {
+    if let indexFile = try? Data(contentsOf: URL(fileURLWithPath: atPath)) {
+
+      log.info("Decoding \(atPath)")
+      let decoder = JSONDecoder()
+      if #available(OSX 10.12, *) {
+        decoder.dateDecodingStrategy = .iso8601
+      }
+
+      if let decodedData = try? decoder.decode(type, from: indexFile) {
+        return decodedData
+      } else {
+        log.error("Could not decode \(atPath)")
+      }
     } else {
-        log.error("File \(atPath) does not exist")
+      log.error("Could not read \(atPath)")
     }
-    return nil
+  } else {
+    log.error("File \(atPath) does not exist")
+  }
+  return nil
 }
